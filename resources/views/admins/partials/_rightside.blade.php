06ce41941660b0d5556bfa67d9abd54266f6e366
M. Ammar Izzudin <zoed.eldien15@gmail.com>
2025-02-22 13:13:14 +0700

    fix: Fix chart data

--- resources/views/admins/partials/_rightside.blade.php
+++ resources/views/admins/partials/_rightside.blade.php
@@ -43,11 +43,11 @@
                             },
                             data: [
                                 {
-                                    value: {{ $genderCounts['P'] }},
+                                    value: {{ $genderCounts['female'] }},
                                     name: '{{ __('Kader Perempuan') }}',
                                 },
                                 {
-                                    value: {{ $genderCounts['L'] }},
+                                    value: {{ $genderCounts['male'] }},
                                     name: '{{ __('Kader Laki-Laki') }}',
                                 },
                             ],
