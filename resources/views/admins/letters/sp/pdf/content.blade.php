39481a7efdd5c39cf52023f20356596356e765f8
M. Ammar Izzudin <zoed.eldien15@gmail.com>
2025-02-24 18:40:36 +0700

    feat: Add generate SP feature

---
+++ resources/views/admins/letters/sp/pdf/content.blade.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Surat Pengesahan PC IPNU</title>
+        <style>
+            /* Ukuran halaman dan margin sesuai format surat */
+            @page {
+                size: A4;
+                margin: 2.5cm 3cm 2.5cm 3cm; /* Atas, kanan, bawah, kiri */
+            }
+
+            body {
+                margin: 0;
+                font-size: 12pt;
+                line-height: 1.5;
+                font-family: 'Arial', sans-serif;
+                text-align: justify;
+            }
+
+            /* Judul dan Nomor Surat */
+            .judul-surat {
+                position: relative;
+                align-items: center;
+                font-weight: bold;
+                font-size: 12pt;
+                text-align: center;
+                text-transform: uppercase;
+            }
+
+            .nomor-surat {
+                margin-bottom: 15px;
+                font-size: 12pt;
+                text-align: center;
+            }
+
+            .about {
+                font-size: 12pt;
+                text-align: center;
+            }
+
+            /* Separator */
+            .separator {
+                margin: 15px 0;
+                border-top: 2px solid black;
+                width: 100%;
+            }
+
+            /* Konten Surat */
+            .content {
+                margin-top: 20px;
+            }
+
+            /* Bagian yang ditampilkan dalam format poin */
+            .section-title {
+                margin-bottom: 5px;
+                font-weight: bold;
+            }
+
+            .section-content {
+                margin-left: 20px;
+            }
+
+            /* Bagian keputusan */
+            .decision {
+                margin: 20px 0;
+                font-weight: bold;
+                font-size: 12pt;
+                text-align: center;
+            }
+
+            /* Identitas Tanda Tangan */
+            .signature {
+                margin-top: 40px;
+                text-align: center;
+            }
+
+            /* Format daftar untuk poin */
+            ol {
+                margin: 0;
+                padding-left: 20px;
+            }
+
+            .lampiran {
+                margin-top: 40px;
+                border-top: 1px solid black;
+                padding-top: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- Judul Surat -->
+        <div class="judul-surat">SURAT PENGESAHAN PC IPNU</div>
+        <div class="nomor-surat">Nomor: 022/PC/SP/XIII/7354/X/24</div>
+        <div class="about">Tentang</div>
+        <div class="judul-surat">
+            SUSUNAN PENGURUS PIMPINAN ANAK CABANG IKATAN PELAJAR NAHDLATUL ULAMA KECAMATAN BATURRADEN KABUPATEN BANYUMAS
+            <br />
+            MASA KHIDMAT 2023-2025
+        </div>
+
+        <!-- Separator -->
+        <div class="separator"></div>
+
+        <!-- Konten Surat -->
+        <div class="content">
+            <p><strong>Bismillahirrahmanirrahim</strong></p>
+            <p>Pimpinan Cabang Ikatan Pelajar Nahdlatul Ulama Kabupaten Banyumas setelah:</p>
+
+            <p class="section-title">Menimbang:</p>
+            <div class="section-content">
+                <ol>
+                    <li>
+                        Ikatan Pelajar Nahdlatul Ulama sebagai organisasi kader yang terus mengalami peningkatan dan
+                        perkembangan baik secara organisatoris maupun program yang dicanangkan, maka perlu terus
+                        dilakukan pembaharuan dan regenerasi pengurus melalui pergantian pengurus secara periodik;
+                    </li>
+                    <li>
+                        Dalam upaya menjalankan kepengurusan untuk semua tingkatan maka diperlukan adanya kesiapan dan
+                        kecakapan pengurus dalam rangka mengantisipasi setiap perubahan dan perkembangan menuju
+                        tercapainya misi dan tujuan organisasi;
+                    </li>
+                    <li>
+                        Bahwa untuk menjalankan kepengurusan PAC IPNU Kecamatan Baturraden, maka perlu menerbitkan Surat
+                        Pengesahan ini.
+                    </li>
+                </ol>
+            </div>
+
+            <p class="section-title">Mengingat:</p>
+            <div class="section-content">
+                <ol>
+                    <li>Peraturan Dasar (PD) IPNU BAB I Pasal 1, BAB VII Pasal 12, BAB VIII Pasal 16;</li>
+                    <li>Peraturan Rumah Tangga (PRT) IPNU Bab IX Pasal 20, Ayat 4;</li>
+                    <li>Peraturan Organisasi IPNU Bab IV Pasal 8.</li>
+                </ol>
+            </div>
+
+            <p class="section-title">Memperhatikan:</p>
+            <div class="section-content">
+                <ol>
+                    <li>Sidang Pleno IPNU Kecamatan Baturraden tanggal 21 Juli 2024;</li>
+                    <li>
+                        Surat Rekomendasi MWC NU Kecamatan Baturraden Nomor: 049/MWCNU/A.I/VII/2024 tanggal 25 Juli
+                        2024;
+                    </li>
+                    <li>Berita Acara Pemilihan Ketua dan Tim Formatur.</li>
+                </ol>
+            </div>
+
+            <div class="decision">M E M U T U S K A N</div>
+
+            <p class="section-title">Menetapkan:</p>
+            <div class="section-content">
+                <ol>
+                    <li>
+                        Mengesahkan susunan Pimpinan Anak Cabang Ikatan Pelajar Nahdlatul Ulama Kecamatan Baturraden,
+                        Masa Khidmat 2023-2025 sebagaimana terlampir;
+                    </li>
+                    <li>
+                        Menugaskan kepada semua pengurus Pimpinan Anak Cabang Ikatan Pelajar Nahdlatul Ulama Kecamatan
+                        Baturraden untuk melaksanakan amanat organisasi, sesuai hasil keputusan konferensi dan peraturan
+                        yang ada;
+                    </li>
+                    <li>
+                        Surat Pengesahan ini berlaku mulai tanggal ditetapkan sampai dengan tanggal 30 Juli 2025 dan
+                        apabila terdapat kekeliruan di kemudian hari akan ditinjau kembali.
+                    </li>
+                </ol>
+            </div>
+
+            <p>Wallahulmuwaffiq ilaa aqwamith-tharieq</p>
+
+            <!-- Identitas Tanda Tangan -->
+            <div class="signature">
+                <p>Ditetapkan di: Purwokerto</p>
+                <p>
+                    Pada tanggal: 11 Rabi’ul Akhir 1446 H
+                    <br />
+                    13 Oktober 2024 M
+                </p>
+                <p>
+                    <strong>PIMPINAN CABANG</strong>
+                    <br />
+                    IKATAN PELAJAR NAHDLATUL ULAMA
+                    <br />
+                    KABUPATEN BANYUMAS
+                </p>
+                <p>Ketua, Sekretaris, Sekretaris,</p>
+                <br />
+                <p><strong>FAHMI ABDURRAHMAN &nbsp;&nbsp;&nbsp; AKHMAD AINUN NAJIB</strong></p>
+                <p>NIA. 11.20.99.00002 &nbsp;&nbsp;&nbsp; NIA. 11.20.99.00032</p>
+            </div>
+        </div>
+
+        <div class="lampiran">
+            <h3>Lampiran Surat Pengesahan</h3>
+            <p>
+                Pimpinan Cabang Ikatan Pelajar Nahdlatul Ulama Kabupaten Banyumas
+                <br />
+                Nomor: 022/PC/SP/XIII/7354/IX/24
+            </p>
+            <h3>
+                SUSUNAN PENGURUS PIMPINAN ANAK CABANG
+                <br />
+                IKATAN PELAJAR NAHDLATUL ULAMA
+                <br />
+                KECAMATAN BATURRADEN KABUPATEN BANYUMAS
+            </h3>
+            <p>MASA KHIDMAT 2023-2025</p>
+            <p>
+                <strong>PELINDUNG:</strong>
+                MWC NU Kecamatan Baturraden
+                <br />
+                <strong>PEMBINA:</strong>
+                <br />
+                1. M. Afandri Awal
+                <br />
+                2. Fathurrohman, S.Pd.
+                <br />
+                3. Suyitno, S.Ag.
+                <br />
+                4. Zainurrochman, S.H.
+                <br />
+                5. Ustadz Agus Setiawan, S.H.I., M.H.
+                <br />
+                6. Akhmad Faiz
+                <br />
+                7. Muhammad Makhdum Wahid
+            </p>
+            <p>
+                <strong>PENGURUS HARIAN:</strong>
+                <br />
+                Ketua: Tri Rafli Ikhsanudin
+                <br />
+                Wakil Ketua: Haydar Firman Jamaludin
+                <br />
+                Wakil Ketua: Sahrul Hidayat
+                <br />
+                Sekretaris: Zulfan Ulinnuha
+                <br />
+                Wakil Sekretaris: Ranto Wijiono
+                <br />
+                Bendahara: Bagus Muwaafiqul Aufa
+                <br />
+                Wakil Bendahara: Divan Anggis Saputra
+            </p>
+            <p>
+                <strong>DEPARTEMEN-DEPARTEMEN:</strong>
+                <br />
+                A. Departemen Organisasi
+                <br />
+                &nbsp;&nbsp;&nbsp; Koordinator: Faizal Adi Purnomo
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Zola Adittya Saputra
+                <br />
+                B. Departemen Kaderisasi
+                <br />
+                &nbsp;&nbsp;&nbsp; Koordinator: M. Niam Hidayat
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Tegar Barep Saputra
+                <br />
+                C. Departemen Dakwah
+                <br />
+                &nbsp;&nbsp;&nbsp; Koordinator: Reza Alfani
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Saefurrohman
+                <br />
+                D. Departemen Olahraga, Seni dan Budaya
+                <br />
+                &nbsp;&nbsp;&nbsp; Koordinator: M. Zidanuddin Hidayat
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Rohalim, Wahyu Adi Saputra, Wahyu Nur Hidayat, Lutfi Hidayat, Razid Fikri
+                Fani Putra
+                <br />
+                E. Departemen Hubungan dan Pengabdian Masyarakat
+                <br />
+                &nbsp;&nbsp;&nbsp; Koordinator: Akhmad Faozi
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: David Amri Solikhin
+            </p>
+            <p>
+                <strong>LEMBAGA-LEMBAGA:</strong>
+                <br />
+                A. Lembaga Ekonomi dan Kewirausahaan
+                <br />
+                &nbsp;&nbsp;&nbsp; Direktur: Muhammad Mukhson
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Dennis Fairliano
+                <br />
+                B. Lembaga Pers dan Penerbitan
+                <br />
+                &nbsp;&nbsp;&nbsp; Direktur: Muhammad Nasiruddin
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Revian Aziana P., Kukuh Prasetya
+                <br />
+                C. Lembaga Corps Brigade Pembangunan
+                <br />
+                &nbsp;&nbsp;&nbsp; DEWAN KOORDINASI ANAK CABANG (DKAC)
+                <br />
+                &nbsp;&nbsp;&nbsp; Koordinator: Ridho Ma’i Pratama
+                <br />
+                &nbsp;&nbsp;&nbsp; Anggota: Sugeng
+            </p>
+            <p>
+                Ditetapkan di: Purwokerto
+                <br />
+                Pada tanggal: 11 Rabi’ul Akhir 1446 H
+                <br />
+                : 13 Oktober 2024 M
+            </p>
+            <p>
+                PIMPINAN CABANG
+                <br />
+                IKATAN PELAJAR NAHDLATUL ULAMA
+                <br />
+                KABUPATEN BANYUMAS
+            </p>
+            <p>Ketua, Sekretaris, Sekretaris,</p>
+            <br />
+            <br />
+            <p>
+                FAHMI ABDURRAHMAN AKHMAD AINUN NAJIB
+                <br />
+                NIA. 11.20.99.00002 &nbsp;&nbsp;&nbsp; NIA. 11.20.99.00032
+            </p>
+        </div>
+    </body>
+</html>
---
+++ resources/views/admins/letters/sp/pdf/cover.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{{ __('Cover SP') }}</title>
+    </head>
+
+    <body></body>
+</html>
