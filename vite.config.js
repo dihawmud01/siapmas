da9b010dd3a6cfa3f6b25a012984829a015e08a5
ASUS M413DA <iammakhdumwahid@gmail.com>
2025-04-11 01:31:56 +0700

    Vite Edit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js', 'resources/css/app.css'],
+            input: [
+                'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/css/admin.css',
+                'resources/css/calendar.css',
+                'resources/css/login.css',
+                'resources/css/sp.css',
+                'resources/css/user.css',
+                'resources/js/plugins/apexcharts.js',
+                'resources/js/plugins/echarts.js',
+                'resources/js/plugins/purecounter.js',
+                'resources/js/plugins/swiper.js',
+            ],
             refresh: true,
         }),
         react(),
